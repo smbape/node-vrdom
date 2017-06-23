db24cec0abc7aab934ba3a83d8ee8be79cb8570a
Stéphane MBAPE <smbape@yahoo.fr>
2017-06-23 11:46:54 +0200

    fix IE test failing

--- patches/browser-patches.js
+++ patches/browser-patches.js
@@ -1,12 +1,14 @@
-if (typeof symbol === "undefined") {
+if (typeof Symbol !== "function" || !Symbol.iterator) {
     require('core-js/fn/symbol');
 }
 
-if (typeof Map === "undefined") {
+var ITERATOR_SYMBOL = typeof Symbol === "function" && Symbol.iterator;
+
+if (typeof Map !== "function" || typeof Map.prototype[ITERATOR_SYMBOL] !== "function") {
     require('core-js/fn/map');
 }
 
-if (typeof Set === "undefined") {
+if (typeof Set !== "function" || typeof Set.prototype[ITERATOR_SYMBOL] !== "function") {
     require('core-js/fn/set');
 }
 
